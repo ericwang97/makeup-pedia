6b09b908424417d73bb7be2bc7c0589c2ef95efa
jiaywan <chloewang_jy@outlook.com>
2021-04-29 20:06:15 -0700

    connect to neo4j

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import json
 import time
+from py2neo import Graph
 
 from Backend.backend_features.reco import reco_main
 from Backend.backend_features.search import search_main
@@ -86,3 +87,60 @@ def rate_execution(rate, comment):
                                       ensure_ascii=False)
     except:
         return json.dumps({'status': 1, 'msg': 'Sorry, sth wrong here..'}, indent=4)
+
+def search_graph_execution(product_id_list):
+    g = Graph(
+        host="127.0.0.1",
+        http_port=7474,
+        user="neo4j",
+        password="makeuppedia")
+
+    try:
+        product_id_list = [int(i) for i in product_id_list]
+        print(product_id_list)
+        query = "match path=(m:Product)-[r]->(n) where m.product_id in $s return relationships(path) as relationships  limit 200"
+        data = g.run(query, s=product_id_list)
+
+        nodes = []
+        rels = []
+
+        node_dict = {}
+
+        while data.forward():
+            cursor = data.current
+            for relation in cursor['relationships']:
+
+                source_node = relation.start_node
+                target_node = relation.end_node
+                source_node_name = source_node['name']
+                target_node_name = target_node['name']
+
+                relation_type = list(relation.types())[0]
+                source_node_label = str(source_node.labels).strip(":")
+                target_node_label = str(target_node.labels).strip(":")
+
+                cur_source_node = {'name': source_node_name, 'label': source_node_label }
+                cur_target_node = {'name': target_node_name, 'label': target_node_label }
+
+                if cur_source_node['name']+"|||"+cur_source_node['label'] not in node_dict.keys():
+                    node_dict[cur_source_node['name']+"|||"+cur_source_node['label']] = len(node_dict)
+                    nodes.append(cur_source_node)
+
+                if cur_target_node['name']+"|||"+cur_target_node['label'] not in node_dict.keys():
+                    node_dict[cur_target_node['name']+"|||"+cur_target_node['label']] = len(node_dict)
+                    nodes.append(cur_target_node)
+
+                cur_source_node['id'] = node_dict[cur_source_node['name']+"|||"+cur_source_node['label']]
+                cur_target_node['id'] = node_dict[cur_target_node['name'] + "|||" + cur_target_node['label']]
+
+                rels.append(
+                    {
+                        'source':  cur_source_node['id'],
+                        'target':  cur_target_node['id'],
+                        'relation': relation_type
+                    }
+                )
+        return json.dumps({'nodes': nodes, 'links': rels}, indent=4)
+
+    except Exception as e:
+        print(e)
